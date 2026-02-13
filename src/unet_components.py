c64c1981ec889b09e614449e660a285bff8167b9
Ariana Hejazyan <arianahejazyan@gmail.com>
2026-02-13 13:38:35 +0330

    feat: implement UpSample module

--- src/unet_components.py
+++ src/unet_components.py
@@ -24,3 +24,14 @@ class DownSample(nn.Module):
         down = self.conv(x)
         p = self.pool(down)
         return down, p
+
+class UpSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.concat([x1, x2], 1)
+        return self.conv(x)
